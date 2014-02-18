48b2849863398519ed8733762fdcc87a90bfb22f
Wes Risenmay <wesrisenmay@gmail.com>
2014-02-18 09:52:30 -0800

    Added basic outline of how data generation will occur

---
+++ src/DataType.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Wes on 2/18/14.
+ */
+public enum DataType {
+    random, circle
+}
---
+++ src/GeneratedTestData.java
@@ -0,0 +1,23 @@
+import javafx.geometry.Point2D;
+
+/**
+ * Created by Wes on 2/18/14.
+ * This class will be provided with an array size N, and it will return several different types of arrays of points to test out several
+ * different cases.
+ */
+public class GeneratedTestData {
+    private Point2D point[];
+
+    //
+    public GeneratedTestData(int count, DataType type) {
+        switch(type) {
+            case circle:
+                //generate circle
+                break;
+            case random:
+                //generate random data
+                break;
+        }
+    }
+}
+
