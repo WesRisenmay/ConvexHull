867f2df8caa8af8ccb482a821b60d309dc6b4dca
Wes Risenmay <wesrisenmay@gmail.com>
2014-02-25 10:44:45 -0800

    Finished speed testing framework

--- src/ConvexHull.java
+++ src/ConvexHull.java
@@ -1,42 +1,54 @@
-import java.util.Dictionary;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by Wes on 2/18/14.
  */
 public class ConvexHull {
     public static void main(String args[]) {
-        //This is how you generate data
-        GeneratedTestData generated = new GeneratedTestData(100, DataType.random);
-        generated.printArray(true);
-        generated = new GeneratedTestData(100, DataType.circle);
-        generated.printArray(true);
-        generated = new GeneratedTestData(99, DataType.circle);
-        generated.printArray(true);
-
-        //This is how you create a list test data to be run
-        LinkedList<TestNumber> testNumbersList = new LinkedList<TestNumber>();
-        testNumbersList.add(new TestNumber(10,50));
-        testNumbersList.add(new TestNumber(100,25));
-        testNumbersList.add(new TestNumber(1000,12));
-        testNumbersList.add(new TestNumber(10000,6));
-        testNumbersList.add(new TestNumber(100000,3));
-        testNumbersList.add(new TestNumber(1000000,1));
+        //This is how you create a list of test data to be run
+        LinkedList<TestNumbers> testNumbersList = new LinkedList<TestNumbers>();
+        testNumbersList.add(new TestNumbers(10,2000000));
+        testNumbersList.add(new TestNumbers(100,2500000));
+        testNumbersList.add(new TestNumbers(1000,1200000));
+        testNumbersList.add(new TestNumbers(10000,600000));
+        testNumbersList.add(new TestNumbers(100000,300000));
+        testNumbersList.add(new TestNumbers(1000000,100000));
 
         //This is how you run the speed tests
-        testSpeed(testNumbersList);
+        System.out.println(testSpeed(testNumbersList));
     }
 
 
 
-    public static String testSpeed(LinkedList<TestNumber> testNumbersList) {
+    public static String testSpeed(LinkedList<TestNumbers> testNumbersList) {
         String total = "";
-        for(TestNumber t: testNumbersList) {
-            //Run brute force
-            //Record data in total
-            //Run quickhull
-            //record data in total
+        for(TestNumbers t: testNumbersList) {
+            //Generate Data
+            GeneratedTestData circle = new GeneratedTestData(t.n, DataType.circle);
+            GeneratedTestData random = new GeneratedTestData(t.n, DataType.random);
+
+            Timestamp lastTime = new Timestamp(new Date().getTime());
+
+            for(int i = 0; i < t.timesToRun; i++) {
+                //Run brute force: replace with actual call
+            }
+
+            Timestamp time = new Timestamp(new Date().getTime());
+            total += "Brute force took " + (time.getTime()-lastTime.getTime()) + " milliseconds to run " + t.timesToRun + " times " +
+                    "with n of size " + t.n + ", averaging " + ((time.getTime()-lastTime.getTime())/t.timesToRun) + " milliseconds.\n";
+
+            lastTime = new Timestamp(new Date().getTime());
+
+            for(int i = 0; i < t.timesToRun; i++) {
+                //Run quick hull: replace with actual call
+            }
+
+            time = new Timestamp(new Date().getTime());
+            total += "Quick hull took " + (time.getTime()-lastTime.getTime()) + " milliseconds to run " + t.timesToRun + " times " +
+                    "with n of size " + t.n + ", averaging " + ((time.getTime()-lastTime.getTime())/t.timesToRun) + " milliseconds.\n\n";
+
         }
 
         return total;
--- src/TestNumbers.java
+++ src/TestNumbers.java
@@ -1,11 +1,11 @@
 /**
  * Created by Wes on 2/25/14.
  */
-public class TestNumber {
+public class TestNumbers {
     public int n;
     public int timesToRun;
 
-    public TestNumber(int n, int timesToRun) {
+    public TestNumbers(int n, int timesToRun) {
         this.n = n;
         this.timesToRun = timesToRun;
     }
